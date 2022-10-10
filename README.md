e4fd190237fb46c4f9a89ab30d65e5c7bf56033b
mikeyy <adhikari.shrish1@gmail.com>
2022-10-10 14:10:43 +0545

    Session working seen

--- README.md
+++ README.md
@@ -9,3 +9,7 @@
 
 - `docker-compose up`
 - `docker exec -it session_main sh`
+
+## what I drew out of this?
+
+- the cookies aren't available on the response when you do `res.cookies` but its actually present on the session
--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
-import requests
+import requests, pickle
+import os
 
 headers = {
     "accept": "application/json"
@@ -14,14 +15,49 @@ login_data = {
     "pw": "tyba"
 }
 
-url_get = "http://httpbin/get"
+url_get = "http://httpbin/cookies"
 # url_post = "https://httpbin.org/post"
 # url_post = "http://localhost:8000/post" # for local
 url_post = "http://httpbin/post" # through container
+def save_cookie(cookie, filename="cookie.txt"):
+    print("Saving cookies")
+    with open(filename, "wb") as f:
+        # pickle.dump(res_cookie.cookies, f)
+        pickle.dump(cookie, f)
 
+def load_cookie(filename="cookie.txt"):
+    with open('cookie.txt', 'rb') as f:
+        print("Loading cookies")
+        new_session.cookies.update(pickle.load(f))
+
+session = requests.session()
 # res = requests.get(url_get, cookies=cookies)
-res = requests.post(url_post, login_data, cookies=cookies)
+# res_cookie = session.get("https://www.google.com/")
+res_cookie = session.get("http://httpbin/cookies/set/sessioncookie/bleach12345'")
 
+
+
+if not os.path.exists('cookie.txt'):
+        save_cookie(session.cookies, "cookie.txt")
+
+new_session = requests.session()
+load_cookie()
+
+res = new_session.post(url_post, login_data)
 print(res.text)
-print(res.cookies)
 print(res.status_code)
+
+print(f"ARE THE COOKIES SAME for post? : {res.cookies == res_cookie.cookies}")
+print(new_session.cookies)
+print(res.cookies)
+
+res = new_session.get(url_get)
+print(res.text)
+print(res.status_code)
+
+print(f"ARE THE COOKIES SAME for get? : {res.cookies == res_cookie.cookies}")
+print(new_session.cookies) # this holds the session cookie
+print(res.cookies) # this shows empty 
+
+print(res.json()) # this will show the session cookies
+
